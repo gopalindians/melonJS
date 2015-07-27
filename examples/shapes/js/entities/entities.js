a363519cd05c3f83d22a30c7f21a1c8d787ed7e2
Olivier Biot <olivier.biot@me.com>
2015-07-28 00:02:02 +0800

    refactored the shapes example to use the new `pointer enter` and `pointerleave`

--- examples/shapes/js/entities/entities.js
+++ examples/shapes/js/entities/entities.js
@@ -7,71 +7,91 @@ game.ShapeObject = me.Entity.extend({
         settings.shapes = [];
         // call the super constructor
         this._super(me.Entity, "init", [x, y, settings]);
+        
+        // hovering flag
         this.hover = false;
-        this.handler = me.event.subscribe("pointermove", this.mouseMove.bind(this));
+        this.selected = false;
 
         // to memorize where we grab the shape
         this.grabOffset = new me.Vector2d(0,0);
 
-        //register on mouse/touch event
+        //register on pointer event
         me.input.registerPointerEvent("pointerdown", this, this.onSelect.bind(this));
+        me.input.registerPointerEvent("pointercancel", this, this.onRelease.bind(this));
         me.input.registerPointerEvent("pointerup", this, this.onRelease.bind(this));
+        me.input.registerPointerEvent("pointerenter", this, this.onEnter.bind(this));
+        me.input.registerPointerEvent("pointerleave", this, this.onLeave.bind(this));
+        
+        //me.input.registerPointerEvent("pointermove", this, this.onMove.bind(this));
+        me.event.subscribe(me.event.MOUSEMOVE, this.onMove.bind(this));
     },
 
     /**
-     * mousemove function
+     * pointerenter function
      */
-    mouseMove: function (event) {
+    onEnter: function (event) {
         this.hover = false;
 
         // the pointer event system will use the object bounding rect, check then with with all defined shapes
-        if (this.inViewport && this.getBounds().containsPoint(event.gameX, event.gameY)) {
-            for (var i = this.body.shapes.length, shape; i--, (shape = this.body.shapes[i]);) {
-                if (shape.containsPoint(event.gameX - this.pos.x, event.gameY - this.pos.y)) {
-                    this.hover = true;
-                    break;
-                }
+        for (var i = this.body.shapes.length, shape; i--, (shape = this.body.shapes[i]);) {
+            if (shape.containsPoint(event.gameX - this.pos.x, event.gameY - this.pos.y)) {
+                this.hover = true;
+                // don't propagate the event furthermore
+                return false;
             }
-        }
-
-        if (this.canMove) {
-            // follow the mouse/finger
-            me.game.world.moveToTop(this);
-            this.pos.set(event.gameX, event.gameY, this.pos.z);
-            this.pos.sub(this.grabOffset);
         }
     },
 
+    // pointer leave function
+    onLeave : function (/*event*/) {
+        this.hover = false;
+    },
 
-    // mouse down function
+
+    // pointer down function
     onSelect : function (event) {
         if (this.hover === true) {
+
+            me.game.world.moveToTop(this);
+
             // the pointer event system will use the object bounding rect, check then with with all defined shapes
             for (var i = this.body.shapes.length, shape; i--, (shape = this.body.shapes[i]);) {
                 if (shape.containsPoint(event.gameX - this.pos.x, event.gameY - this.pos.y)) {
                     this.grabOffset.set(event.gameX, event.gameY);
                     this.grabOffset.sub(this.pos);
-                    this.canMove = true;
-                    // don"t propagate the event furthermore
+                    this.selected = true;
+                    // don't propagate the event furthermore
                     return false;
                 }
             }
-            return true;
         }
     },
 
     // mouse up function
     onRelease : function (/*event*/) {
-        this.canMove = false;
-        // don"t propagate the event furthermore
-        return false;
+        if (this.selected === true) {
+            this.selected = false;
+            // don't propagate the event furthermore
+            return false;
+        }
+    },
+
+    /**
+     * pointermove function
+     */
+    onMove: function (event) {
+        if (this.selected === true) {
+            // follow the mouse/finger
+            this.pos.set(event.gameX, event.gameY, this.pos.z);
+            this.pos.sub(this.grabOffset);
+        }
     },
 
     /**
      * update function
      */
     update: function () {
-        return true;
+        return this.hover || this.selected;
     },
 
     /**
